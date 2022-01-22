9fc48392906e5be089634a7cd1334fdd072023cd
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2022-01-22 11:04:00 +0900

    Feat: add blog info name & description

--- wordpress/wp-content/themes/clean-blog/index.php
+++ wordpress/wp-content/themes/clean-blog/index.php
@@ -44,8 +44,8 @@
       <div class="row gx-4 gx-lg-5 justify-content-center">
         <div class="col-md-10 col-lg-8 col-xl-7">
           <div class="site-heading">
-            <h1>Clean Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1><?php bloginfo('name'); ?></h1>
+            <span class="subheading"><?php bloginfo('description'); ?></span>
           </div>
         </div>
       </div>
