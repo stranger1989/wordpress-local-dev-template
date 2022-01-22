dd561e67259bfdf6b12f415979867f834aab173e
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2022-01-22 10:55:48 +0900

    Feat: display regular post list

--- wordpress/wp-content/themes/clean-blog/index.php
+++ wordpress/wp-content/themes/clean-blog/index.php
@@ -56,61 +56,23 @@
     <div class="row gx-4 gx-lg-5 justify-content-center">
       <?php if (have_posts()) : ?>
         <div class="col-md-10 col-lg-8 col-xl-7">
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-              <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on September 24, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on September 18, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Science has not yet mastered prophecy</h2>
-              <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on August 24, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Failure is not an option</h2>
-              <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on July 8, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
+          <?php while (have_posts()) : the_post(); ?>
+            <div class="post-preview">
+              <a href="<?php the_permalink() ?>">
+                <h2 class="post-title">
+                  <?php the_title(); ?><br />
+                </h2>
+                <h3 class="post-subtitle">
+                  <?php the_excerpt(); ?><br />
+                </h3>
+              </a>
+              <p class="post-meta">Posted by
+                <?php the_author() ?>
+                on September 24, 2019 <?php the_time(get_option('date_format')); ?>
+              </p>
+            </div>
+            <hr>
+          <?php endwhile; ?>
           <!-- Pager-->
           <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
         </div>
