2abaafc27367720e5b1b9b338d74afc0900da502
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2022-01-23 07:39:50 +0900

    Fix: default image directory path

--- wordpress/wp-content/themes/clean-blog/functions.php
+++ wordpress/wp-content/themes/clean-blog/functions.php
@@ -9,8 +9,7 @@ function get_eye_catch_with_default()
     $id = get_post_thumbnail_id();
     $img = wp_get_attachment_image_src($id, 'large');
   else :
-    $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
-    var_dump($img);
+    $img = array(get_template_directory_uri() . '/assets/img/post-bg.jpg');
   endif;
   return $img;
 }
